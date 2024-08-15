be1d3cce6a94a759f1ccf75e30031c58d867f014
Dylan <dpkatsch@gmail.com>
2024-08-15 10:08:44 +0200

    initial commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, stdError} from "lib/forge-std/src/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
@@ -9,16 +9,28 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testInc() public {
+        counter.inc();
+        assertEq(counter.count(), 1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFailDec() public {
+        counter.dec();
+    }
+
+    function testDecUnderflow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc(); // .count is now = 1
+        counter.inc(); // .count is now = 2
+        console.log("count after inc calls", counter.count());
+        counter.dec(); // .count is now = 0
+        console.log("count after dec call", counter.count());
+        assertEq(counter.count(), 1);
     }
 }
