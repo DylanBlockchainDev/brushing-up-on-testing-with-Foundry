5a224179a355294c69089e009f8a8b8b9170d555
Dylan <dpkatsch@gmail.com>
2024-08-15 10:36:05 +0200

    learned about forge fmt

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.20;
 contract Counter {
     uint256 public count;
 
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
     function inc() external {
         count += 1;
     }
