af00a26d77a5cd66d733eb4f2ee60abbf90835a3
Dylan <dpkatsch@gmail.com>
2024-08-15 12:45:47 +0200

    added a few more test files for learning

---
+++ test/Auth.t.sol
@@ -0,0 +1,46 @@
+// PSDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+import {Wallet} from "../src/Wallet.sol";
+
+// forge test --match-path test/Auth.t.sol -vvvv
+
+contract AuthTest is Test {
+    Wallet public wallet;
+
+    function setUp() public {
+        wallet = new Wallet();
+    }
+
+
+    function testSetOwner() public {
+        wallet.setOwner(address(1));
+        assertEq(wallet.owner(), address(1));
+    }
+
+    function testFailNotOwner() public {
+        vm.prank(address(1));
+        wallet.setOwner(address(1));
+    }
+
+    function testFailSetOwnerAgain() public {
+        // msg.sender = address(this)
+        wallet.setOwner(address(1));
+
+        vm.startPrank(address(1));
+
+        // msg.sender = address(1)
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+
+        vm.stopPrank();
+
+        // msg.sender = address(this)
+        wallet.setOwner(address(1));
+    }
+}
