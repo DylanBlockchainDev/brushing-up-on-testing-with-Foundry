366fafa0b139335e24d8ef50659c240a64357290
Dylan <dpkatsch@gmail.com>
2024-08-16 11:12:54 +0200

    Mint 1 Million dai

---
+++ test/Whale.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test, console} from "../lib/forge-std/src/Test.sol";
+import "../src/interfaces/IERC20.sol";
+
+contract ForkTest is Test {
+    IERC20 public dai;
+
+    function setUp() public {
+        dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    }
+
+    function testDeposit() public {
+        address alice = address(123);
+
+        uint balBefore = dai.balanceOf(alice);
+        console.log("balance before", balBefore / 1e18);
+
+        uint256 totalBefore = dai.totalSupply();
+        console.log("total supply before", totalBefore / 1e18);
+
+        deal(address(dai), alice, 1e6 * 1e18, true);
+
+        uint balAfter = dai.balanceOf(alice);
+        console.log("balance after", balAfter / 1e18);
+
+        uint256 totalAfter = dai.totalSupply();
+        console.log("total supply after", totalAfter / 1e18);
+    }
+}
